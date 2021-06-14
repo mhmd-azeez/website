ec12fa9f5ca90278dc231c88bc8ef4b2783527cd
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:36:14 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 BASEDIR=$(dirname "$0")
 
 echo "Installing .NET Core 2.1 SDK..."
-wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh -q
-chmod +x ./dotnet-install.sh
+wget -q https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+chmod +x ./dotnet-install.sh > /dev/null 2>&1 # Install .NET quitely
 ./dotnet-install.sh -Channel 2.1
 
 echo "Installing Wyam..."
-wget https://github.com/Wyamio/Wyam/releases/download/v2.2.9/Wyam-v2.2.9.zip  -q
-unzip Wyam-v2.2.9.zip -d wyam -q
+wget -q https://github.com/Wyamio/Wyam/releases/download/v2.2.9/Wyam-v2.2.9.zip
+unzip -q Wyam-v2.2.9.zip -d wyam
 
 echo "Builiding website..."
 dotnet ./wyam/Wyam.dll --output $BASEDIR/../docs
