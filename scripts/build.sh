461e3b7f275112e48acabb182e02b9875b526c9b
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:12:58 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,8 @@
 BASEDIR=$(dirname "$0")
 
-wget https://download.visualstudio.microsoft.com/download/pr/66483281-5453-4504-9686-f72d1fd357f7/0de98386ca1709bde2b5f2f7df4c80da/aspnetcore-runtime-2.1.28-linux-x64.tar.gz
-mkdir dotnet
-tar -C ./dotnet -zxvf aspnetcore-runtime-2.1.28-linux-x64.tar.gz
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh install.sh
+chmod +x install.sh
+install.sh
 
-export DOTNET_ROOT=$BASEDIR/dotnet
-
-./dotnet/dotnet tool install -g Wyam.Tool
+dotnet tool install -g Wyam.Tool
 ~/.dotnet/tools/wyam --output $BASEDIR/../docs
