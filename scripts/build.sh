41410f5c13af4d9d3581d0624f969cbb98b1caf3
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 20:55:28 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 BASEDIR=$(dirname "$0")
 
 wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 
-apt-get update; \
-apt-get install -y apt-transport-https && \
-apt-get update && \
-apt-get install -y dotnet-sdk-2.1
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-2.1
 
 dotnet tool install -g Wyam.Tool
 ~/.dotnet/tools/wyam --output $BASEDIR/../docs
