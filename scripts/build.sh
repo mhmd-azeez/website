3606408c7c1258a031d07abf9a0d3fcde512abe3
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:31:35 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,6 @@ chmod +x ./dotnet-install.sh
 ./dotnet-install.sh -Channel 2.1
 
 wget https://github.com/Wyamio/Wyam/releases/download/v2.2.9/Wyam-v2.2.9.zip
-unzip Wyam-v2.2.9.zip wyam
+unzip Wyam-v2.2.9.zip -d wyam
 
 dotnet ./wyam/wyam.dll --output $BASEDIR/../docs
