7ad78474d3f28642d4a69f3009b202fa8d0f3ea3
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:09:06 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ BASEDIR=$(dirname "$0")
 
 wget https://download.visualstudio.microsoft.com/download/pr/66483281-5453-4504-9686-f72d1fd357f7/0de98386ca1709bde2b5f2f7df4c80da/aspnetcore-runtime-2.1.28-linux-x64.tar.gz
 mkdir dotnet
-dotnet tar -C ./dotnet -zxvf aspnetcore-runtime-2.1.28-linux-x64.tar.gz
+tar -C ./dotnet -zxvf aspnetcore-runtime-2.1.28-linux-x64.tar.gz
 
 export DOTNET_ROOT=$BASEDIR/dotnet
 
