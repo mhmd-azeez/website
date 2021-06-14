bd315096525f7753733327d2846017fa2dc0ceee
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:15:06 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 BASEDIR=$(dirname "$0")
 
-wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh install.sh
-chmod +x install.sh
-install.sh
+wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh
 
 dotnet tool install -g Wyam.Tool
 ~/.dotnet/tools/wyam --output $BASEDIR/../docs
