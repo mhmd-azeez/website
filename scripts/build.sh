76ecad720b6b5eaa3e3cfe07297de0f24668ea34
Muhammad Azeez <muhammad-azeez@outlook.com>
2021-06-14 21:27:55 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@ chmod +x ./dotnet-install.sh
 ./dotnet-install.sh -Channel 2.1
 
 dotnet tool install Wyam.Tool --framework netcoreapp2.1 --tool-path ./wyam
+ls ./wyam
 dotnet ./wyam/wyam --output $BASEDIR/../docs
